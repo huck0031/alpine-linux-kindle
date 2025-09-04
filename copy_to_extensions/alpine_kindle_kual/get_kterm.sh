9b708d61ff465c61745582bf54e48207f7cfa457
Olivia Huckabone <huck0031@algonquinlive.com>
2025-09-04 14:42:00 -0400

    Add copy_to_extensions

---
+++ copy_to_extensions/alpine_kindle_kual/get_kterm.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+TMP_DOWNLOAD="/tmp/kterm-kindle.zip"
+FOLDER_EXTENSIONS="/mnt/us/extensions/"
+
+KTERM_URL=$(curl -s https://api.github.com/repos/bfabiszewski/kterm/releases/latest \
+  | grep browser_download_url \
+  | grep kterm \
+  | grep -v .zip.sig)
+
+if uname -m | grep -q armhf; then
+	KTERM_URL=$(echo "$KTERM_URL" | grep armhf)
+else
+	KTERM_URL=$(echo "$KTERM_URL" | grep -v armhf)
+fi
+
+KTERM_URL=$(echo "$KTERM_URL" | head -1 | cut -d '"' -f 4)
+
+echo "Downloading kterm from $KTERM_URL"
+if curl -L "$KTERM_URL" --output $TMP_DOWNLOAD ; then
+	# success
+	echo "Extracting to $FOLDER_EXTENSIONS"
+	unzip -q -o $TMP_DOWNLOAD -d $FOLDER_EXTENSIONS
+	rm $TMP_DOWNLOAD
+	echo "kterm installed"
+else
+	# failed
+	echo "Failed to download kterm from $KTERM_URL"
+fi;
+echo "DONE."
